3c10713ce4125be27cc2efeeccf7f3d5d2255e8e
brewchetta <chett.tiller@gmail.com>
2024-10-11 13:39:36 -0400

    cats cats cats

--- index.js
+++ index.js
@@ -1,33 +1,91 @@
 // ASYNC JAVASCRIPT //
 
-// NUMBER ONE
-console.log("Hello I am synchronous!")
+// let something = "SOMETHING"
 
-// NUMBER FOUR (after 5000 milliseconds)
-setTimeout(() => console.log("I am 5000"), 5000) // <---- asynchronous code
+// // NUMBER ONE
+// console.log("1. Hello I am synchronous!")
 
-setTimeout(() => console.log("I am 100"), 100) // <---- asynchronous code
+// // NUMBER FOUR (after 5000 milliseconds)
+// const timeout = setTimeout(() => console.log(something), 10000) // <---- asynchronous code
+// console.log(timeout)
 
-// NUMBER TWO
-console.log("Hello I am ALSO synchronous!")
+// setTimeout(() => something = "SOMETHING ELSE THAT ISNT THE FIRST THING", 100) // <---- asynchronous code
 
-// NUMBER THREE
-console.log("Hello also synchronous")
+// // NUMBER TWO
+// console.log("4. Hello I am ALSO synchronous!")
 
-// for (let i = 0; i < 100000; i++) {
-//     console.log(i)
-// }
+// // NUMBER THREE
+// console.log("5. Hello also synchronous")
+
+// // for (let i = 0; i < 1000000; i++) {
+// //     console.log(i)
+// // }
 
 // FETCH //
+
+const dadJokesId = document.querySelector('#dad-jokes-container')
+dadJokesId.querySelector('p').remove()
+// document.querySelector('#dad-jokes-id p').remove()
+const newJokeButton = document.querySelector('#new-joke-button')
+
+function fetchDadJoke() {
+    fetch('https://icanhazdadjoke.com/', {
+        headers: { 'Accept': `application/json` }
+    }) // wait until promise is fulfilled then take the response and put it in .then
+    .then((response) => response.json())
+    .then((bodyData) => addsDadJokeToHTML(bodyData.joke))
+}
+
+function addsDadJokeToHTML(dadJoke) {
+    const pTag = document.createElement('p')
+    pTag.textContent = dadJoke
+    dadJokesId.append(pTag)
+}
+
+newJokeButton.addEventListener("click", fetchDadJoke)
 
 // WE WANT JSON
 // Java Script Object Notation
 
-// Make an initial fetch request to the Dad Jokes API and add a new paragraph tag to the `dad-jokes-container` with the new joke.
+
+
+// CAT FACTS
+
+const catFactButton = document.querySelector('#new-cat-fact-button')
+const catFactContainer = document.querySelector('#cat-facts-container')
+let i = 0
+
+function getCatFacts() {
+    fetch('https://cat-fact.herokuapp.com/facts')
+    .then(response => response.json())
+    .then(data => addCatFact(data[i].text))
+}
+
+function addCatFact(catFact) {
+    const pTag = document.createElement('p')
+    pTag.textContent = catFact
+    catFactContainer.append(pTag)
+    if (i < 4) {
+        i++
+    } else {
+        i = 0
+    }
+}
+
+catFactButton.addEventListener("click", getCatFacts)
+
 
 //   - HINT: Be sure to look at the data you get back so you know how to get the joke inside the response...
 
 
 // Start up json-server with `json-sever db.json`. If you aren't able then attempt to install the json-server (installation depends on your OS). You can now fetch jokes from `http://localhost:3000/jokes`. Fetch the array of jokes, iterate through the array, and create a new paragraph tag for each joke just like above.
 
-//   - HINT: Once again make sure you look at the data you get back, it WILL be different from the dad jokes API
+
+
+fetch('http://localhost:3000/jokes')
+.then(res => res.json())
+.then(data => console.log(data))
+
+
+
+//   - HINT: Once again make sure you look at the data you get back, it WILL be different from the dad jokes APIs
